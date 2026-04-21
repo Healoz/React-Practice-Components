c78aa703d355261141fa438458c9507e71be6d63
Healoz <75352596+Healoz@users.noreply.github.com>
2026-04-22 08:18:24 +1000

    Completed basic movie search functionality with animations

---
+++ app/common/LoadingIndicator/index.tsx
@@ -0,0 +1,73 @@
+import { FC } from "react";
+import styles from "./style.module.scss";
+import { motion } from "framer-motion";
+
+interface Props {
+  width?: number;
+}
+
+const LoadingIndicator: FC<Props> = ({ width = 48 }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width={width}>
+      <rect
+        fill="#F8320B"
+        stroke="#F8320B"
+        strokeWidth="4"
+        width="30"
+        height="30"
+        x="25"
+        y="50"
+      >
+        <animate
+          attributeName="y"
+          calcMode="spline"
+          dur="2"
+          values="50;120;50;"
+          keySplines=".5 0 .5 1;.5 0 .5 1"
+          repeatCount="indefinite"
+          begin="-.4"
+        ></animate>
+      </rect>
+      <rect
+        fill="#F8320B"
+        stroke="#F8320B"
+        strokeWidth="4"
+        width="30"
+        height="30"
+        x="85"
+        y="50"
+      >
+        <animate
+          attributeName="y"
+          calcMode="spline"
+          dur="2"
+          values="50;120;50;"
+          keySplines=".5 0 .5 1;.5 0 .5 1"
+          repeatCount="indefinite"
+          begin="-.2"
+        ></animate>
+      </rect>
+      <rect
+        fill="#F8320B"
+        stroke="#F8320B"
+        strokeWidth="4"
+        width="30"
+        height="30"
+        x="145"
+        y="50"
+      >
+        <animate
+          attributeName="y"
+          calcMode="spline"
+          dur="2"
+          values="50;120;50;"
+          keySplines=".5 0 .5 1;.5 0 .5 1"
+          repeatCount="indefinite"
+          begin="0"
+        ></animate>
+      </rect>
+    </svg>
+  );
+};
+
+export default LoadingIndicator;
--- app/components/MovieSearch/index.tsx
+++ app/components/MovieSearch/index.tsx
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from "react";
 import styles from "./style.module.scss";
 import Image from "next/image";
+import LoadingIndicator from "@/app/common/LoadingIndicator";
+import { AnimatePresence, motion, scale } from "framer-motion";
 
 // User Story:
 
@@ -13,19 +15,54 @@ import Image from "next/image";
 // Results display as the user searches or on submit
 // Each result shows the movie title, release year, and poster
 // If no results are found, a friendly message is shown
+// On hover of movie, display name and year. have cool mouse hover affect that shows the name and title on the mouse cursor in a circle
+
+type Movie = {
+  id: string;
+  name: string;
+  year: string;
+  poster: string;
+};
 
 const MovieSearch = () => {
   const [searchText, setSearchText] = useState("");
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [brokenPosters, setBrokenPosters] = useState<Set<string>>(new Set());
+  const [isLoading, setIsLoading] = useState(false);
+  const [isTyping, setIsTyping] = useState(false);
+  const noMoviesFound =
+    movies.length === 0 && searchText.length > 0 && !isLoading && !isTyping;
 
   useEffect(() => {
+    setIsTyping(true);
+
     const timeout = setTimeout(async () => {
       if (searchText) {
-        // fetch movies here
-        console.log("searching for:", searchText);
-        const res = await fetch(`/api/movies?query=${searchText}`);
-        const data = await res.json();
-        console.log(data);
+        setIsTyping(false);
+        setIsLoading(true);
+        setBrokenPosters(new Set());
+        try {
+          const res = await fetch(`/api/movies?query=${searchText}`);
+          const data = await res.json();
+          if (data.Response === "False") {
+            setMovies([]);
+            return;
+          }
+
+          const search = data.Search;
+          setMovies(
+            search.map((m: any) => ({
+              id: m.imdbID,
+              name: m.Title,
+              year: m.Year,
+              poster: m.Poster,
+            })),
+          );
+        } finally {
+          setIsLoading(false);
+        }
+      } else {
+        setIsTyping(false);
       }
     }, 500);
 
@@ -35,43 +72,87 @@ const MovieSearch = () => {
   return (
     <section className={styles.container}>
       <h2>Movie Search</h2>
-      <input
-        type="text"
-        name="search-bar"
-        placeholder="Search"
-        value={searchText}
-        onChange={(e) => setSearchText(e.target.value)}
-      />
-      <div className={styles.movieGrid}>
-        <div className={styles.movieCell}>
-          <Image
-            src="https://m.media-amazon.com/images/M/MV5BMTQyMTQyMzEyNF5BMl5BanBnXkFtZTgwMTI1NjI4MDE@._V1_SX300.jpg"
-            alt="Movie poster"
-            fill
-          />
-        </div>
-        <div className={styles.movieCell}>
-          <Image
-            src="https://m.media-amazon.com/images/M/MV5BMTQyMTQyMzEyNF5BMl5BanBnXkFtZTgwMTI1NjI4MDE@._V1_SX300.jpg"
-            alt="Movie poster"
-            fill
-          />
-        </div>
-        <div className={styles.movieCell}>
-          <Image
-            src="https://m.media-amazon.com/images/M/MV5BMTQyMTQyMzEyNF5BMl5BanBnXkFtZTgwMTI1NjI4MDE@._V1_SX300.jpg"
-            alt="Movie poster"
-            fill
-          />
-        </div>
-        <div className={styles.movieCell}>
-          <Image
-            src="https://m.media-amazon.com/images/M/MV5BMTQyMTQyMzEyNF5BMl5BanBnXkFtZTgwMTI1NjI4MDE@._V1_SX300.jpg"
-            alt="Movie poster"
-            fill
-          />
-        </div>
+      <div className={styles.searchBar}>
+        <input
+          type="text"
+          name="search-bar"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <AnimatePresence>
+          {isLoading && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.2 }}
+            >
+              <LoadingIndicator />
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
+      <AnimatePresence mode="wait">
+        {!noMoviesFound ? (
+          <motion.div
+            className={styles.movieGrid}
+            key="grid"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            {movies.map((movie, i) => (
+              <div className={styles.cellContainer} key={movie.id}>
+                <motion.div
+                  className={styles.movieCell}
+                  initial={{ y: "105%" }}
+                  whileInView={{
+                    y: 0,
+                    transition: {
+                      duration: 0.5,
+                      delay: i * 0.05 + 0.2,
+                      ease: [0.33, 1, 0.68, 1],
+                    },
+                  }}
+                  viewport={{ once: true, margin: "0px 0px 500px 0px" }}
+                  whileHover={{
+                    scale: 1.05,
+                    transition: {
+                      duration: 0.2,
+                      ease: [0.33, 1, 0.68, 1],
+                      type: "tween",
+                    },
+                  }}
+                >
+                  {movie.poster === "N/A" || brokenPosters.has(movie.id) ? (
+                    <p>{movie.name}</p>
+                  ) : (
+                    <Image
+                      src={movie.poster}
+                      alt={`Poster for ${movie.name}`}
+                      fill
+                      onError={() =>
+                        setBrokenPosters((prev) => new Set(prev).add(movie.id))
+                      }
+                    />
+                  )}
+                </motion.div>
+              </div>
+            ))}
+          </motion.div>
+        ) : (
+          <motion.div
+            className={styles.noMoviesFound}
+            key="no-results"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <h3>No movies found</h3>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </section>
   );
 };
