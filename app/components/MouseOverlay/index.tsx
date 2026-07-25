b241918d674147f9226b31cc0b6fba60929b0f3a
Healoz <75352596+Healoz@users.noreply.github.com>
2026-07-25 18:17:43 +1000

    Creating mouse effect

--- app/components/MouseOverlay/index.tsx
+++ app/components/MouseOverlay/index.tsx
@@ -4,6 +4,7 @@ import styles from "./style.module.scss";
 import {
   motion,
   MotionValue,
+  useMotionValue,
   useMotionValueEvent,
   useScroll,
   useTransform,
@@ -12,7 +13,29 @@ import {
 // Inspired by: https://neuemontreal.com/?utm_source=extension&utm_medium=click&utm_campaign=muzli
 
 const MouseOverlay = () => {
-  return <section></section>;
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      mouseX.set(e.clientX - 32);
+      mouseY.set(e.clientY - 32);
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [mouseX, mouseY]);
+  return (
+    <section className={styles.wrapper}>
+      <motion.div
+        className={styles.overlay}
+        style={{ x: mouseX, y: mouseY }}
+      ></motion.div>
+    </section>
+  );
 };
 
 export default MouseOverlay;
--- app/mouse-overlay/page.tsx
+++ app/mouse-overlay/page.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import PlantWateringTracker from "../components/PlantWateringTracker";
 import MouseOverlay from "../components/MouseOverlay";
+
+// Effect of mouse creating an overlay effect on top of elements (usually it inverts things)
 
 const page = () => {
   return <MouseOverlay />;
